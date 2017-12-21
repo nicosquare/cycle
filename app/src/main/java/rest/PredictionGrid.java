568c3f4a7078d25d6302a23a4282dbf0d61a12c6
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2017-12-21 16:59:15 -0500

    Project reestructuration
    The main app is in teh folder /app. Other code in an independent folder.

---
+++ app/src/main/java/rest/PredictionGrid.java
@@ -0,0 +1,46 @@
+package rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+
+import com.google.gson.Gson;
+
+@ApplicationPath("api")
+@Path("/predictionGrid")
+public class PredictionGrid extends Application {
+	
+	
+  /**
+   * Get prediction of grid use.
+   * REST API example:
+   * <code>
+   * GET http://localhost:9080/Cycle/api/predictionGrid
+   * </code>
+   * 
+   * Response:
+   * @return A collection of all the Visitors
+   */
+    @GET
+    @Path("/")
+    @Produces({"application/json"})
+    public String getVisitors() {
+		
+		
+		List<String> names = new ArrayList<String>();
+		
+		names.add("grid");
+		names.add("battery");
+		
+		return new Gson().toJson(names);
+    }
+
+}
+
+
+
