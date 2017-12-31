246801cfd7670b1d33cea96f6df86ef4e7848325
Nicolas Cuadrado <nicolas.cuadrado@hotmail.com>
2017-12-30 21:29:11 -0500

    General fixes

--- app/server.js
+++ app/server.js
@@ -31,33 +31,22 @@ app.use(bodyParser.json())
 
 // UPort code from here
 
-app.post('/authenticate', function (req, res) {
-    
- 	const uport = new Connect('Cycle', {
+const uport = new Connect('Cycle', {
       clientId: '2ojxCynUCy1VWqWJNJSVFAoRRyCPZDkSPw1',
       network: 'rinkeby',
       signer: SimpleSigner('e42efa79fadf96bfb9e9c4b0f75ea010640f55adcdc3cbdfa13638fe361d923d')
     })
 
+app.post('/authenticate', function (req, res) {
+    
     // Request credentials to login
     uport.requestCredentials({
       requested: ['name', 'phone', 'country'],
       notifications: true // We want this if we want to recieve credentials
     })
     .then((credentials) => {
-      // Do something
+      console.log("Something good");
     })
-
-    // Attest specific credentials
-    uport.attestCredentials({
-      sub: THE_RECEIVING_UPORT_ADDRESS,
-      claim: {
-        CREDENTIAL_NAME: CREDENTIAL_VALUE
-      },
-      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
-    })
-
-    console.log('works');
 });
 
 
